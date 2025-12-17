51fca87fea713b0b17e899bb83567ffad1a95ea3
kleakkola <sbhssg26356@gmail.com>
2025-12-17 16:20:00 +0800

    Extend theme colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--bg-primary)",
+        secondary: "var(--bg-secondary)",
+      },
+    },
   },
   plugins: [],
 };
